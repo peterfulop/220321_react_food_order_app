f8a79b3078a858423a81014eda1a71375d4a245d
peterfulop <mrpeterfulop@gmail.com>
2022-03-21 19:56:59 +0100

    feat(Modal.js):created Modal component;

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
@@ -11,7 +12,7 @@ const Cart = (props) => {
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div>
         <span className={classes.total}>Total Amount</span>
@@ -21,7 +22,7 @@ const Cart = (props) => {
         <button className={classes["button--alt"]}>Close</button>
         <button className={classes["button"]}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
