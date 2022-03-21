5d25a18fc6de2c24a6ad66a14ab68372a2138355
peterfulop <mrpeterfulop@gmail.com>
2022-03-21 21:07:59 +0100

    feat(*): Cart modals state implemented;

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsVisible, setCartIsVisible] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsVisible(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsVisible(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsVisible && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
